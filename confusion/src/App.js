d694c38474f7e9a31ebb1365da36051a1ccf9e0e
PranavN0499 <pranav.nagrani@gmail.com>
2020-07-15 19:22:51 +0530

    Commit

---
+++ confusion/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import Main from './components/Maincomponent'
+import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+
+class App extends Component {
+
+  render() {
+    return (
+      <BrowserRouter>
+      <div>
+        <Main />
+      </div>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
